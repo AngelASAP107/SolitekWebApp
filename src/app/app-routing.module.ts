c67341fe893389616b65b4b641ba42befaa38f94
AngelASAP107 <angel.castaneda@correunivalle.edu.co>
2024-05-14 22:46:39 -0500

    Creación del front de gestor de tickets (admin)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register-user/register-user.component';
 import { RegisterComputerComponent } from './auth/register-computer/register-computer.component';
 import { MenuAdminComponent } from './auth/menu-admin/menu-admin.component';
+import { ManageTicketsAdComponent } from './auth/manage-tickets-ad/manage-tickets-ad.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'register-computer', component: RegisterComputerComponent },
   { path: 'menu-admin', component: MenuAdminComponent },
+  {path: 'manage-tickets-ad', component: ManageTicketsAdComponent}
 ];
 
 @NgModule({
